eb4edf0334574f2ec2282fb60a5a6d273fcff5ac
JISOO2206 <900522z@gmail.com>
2023-01-15 22:50:08 +0900

    전체명언보기 클릭시 화면 전환 기능 추가, 앱 타이틀 제거

--- app/src/main/java/kr/co/lifequotesapp/MainActivity.kt
+++ app/src/main/java/kr/co/lifequotesapp/MainActivity.kt
@@ -1,11 +1,25 @@
 package kr.co.lifequotesapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import kr.co.lifequotesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.showAll.setOnClickListener {
+
+            val intent = Intent(this, SentenceActivity::class.java)
+
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/kr/co/lifequotesapp/SentenceActivity.kt
+++ app/src/main/java/kr/co/lifequotesapp/SentenceActivity.kt
@@ -1,11 +1,16 @@
 package kr.co.lifequotesapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import kr.co.lifequotesapp.databinding.ActivityMainBinding
 
 class SentenceActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sentence)
+
     }
 }
